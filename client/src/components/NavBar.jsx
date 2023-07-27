233ee5278796eaf7a4f2f9c37e056ccea4d1ecb6
javimt <javiergp_19@outlook.com>
2023-07-27 18:37:30 -0500

    navbar responsive

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,26 +1,37 @@
-
+import { useState } from "react";
 import { Link } from "react-scroll";
 import { GiHamburgerMenu, GiCrossedBones } from "react-icons/gi";
 import image from "../assets/rent apt.jpeg";
 import styles from "../styles/Navbar.module.css";
 
 const NavBar = () => {
-  
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
-    <nav className={styles.nav}>
-      <img src={image} alt="amoblados" className={styles.image} />
-      <Link to="/" className={styles.link}>
-        <div className={styles.home}>Home</div>
-      </Link>
-      <Link to="apartaments" className={styles.link}>
-        <div className={styles.apartaments}>Apartaments</div>
-      </Link>
-      <Link to="about" className={styles.link}>
-        <div className={styles.about}>About Us</div>
-      </Link>
-      <div className={styles.login}>Login</div>
-    </nav>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} ${showMenu ? styles.showMenu : ""}`}>
+        <img src={image} alt="amoblados" className={styles.image} />
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>
+          <Link to="apartaments" className={styles.link}>
+            Apartaments
+          </Link>
+          <Link to="about" className={styles.link}>
+            About Us
+          </Link>
+          <div className={styles.login}>Login</div>
+          {showMenu ? (
+          <GiCrossedBones className={styles.closeIcon} onClick={handleMenu} />
+        ) : (
+          <GiHamburgerMenu className={styles.menuIcon} onClick={handleMenu} />
+        )}
+      </nav>
+    </header>
   );
 };
 
