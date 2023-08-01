155bef223db684efdef9d6fa1ddc7a97148a6d36
javimt <javiergp_19@outlook.com>
2023-08-01 15:42:52 -0500

    contacts ok

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
     <div className={styles.footer}>
       <div className={styles.by}>
         <p className={styles.p}> Copyright © 2023. </p>
-        <p className={styles.p}> by www.furnishedapartamentmedellin.com.  All right reserved</p>
+        <p className={styles.p}> www.furnishedapartmentmedellin.com</p>
+        <p className={styles.p}> All right reserved</p>
       </div>
     </div>
   )
