2739f6606bad564d5a327714e6cd9c42ccfe9a2f
javimt <javiergp_19@outlook.com>
2023-09-30 14:53:59 -0500

    fix footer link url ok

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.by}>
         <p className={styles.p}> Copyright © 2023. </p>
-        <a href="http://www.furnishedapartmentmedellin.com" className={styles.a}>
+        <a href="http://www.furnishedapartmentsmedellin.com" className={styles.a}>
           <p className={styles.p}> www.furnishedapartmentmedellin.com</p>
         </a>
         <p className={styles.p}> All right reserved</p>
