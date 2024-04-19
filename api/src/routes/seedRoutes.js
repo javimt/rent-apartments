6d68e8cec1708da797e91479120ad6e91d63fcd8
nestornovella <importarnorth@gmail.com>
2024-04-19 11:58:44 -0300

    changes

--- api/src/routes/seedRoutes.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express')
-const { seedUser, seedApartment} = require('../controllers/seedDB')
-
-const router = Router()
-
-router.get('/user', seedUser )
-router.get('/apartment', seedApartment)
-
-module.exports = router
