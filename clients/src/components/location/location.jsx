b5afea36c0602fa11c5f78ef3cbf5cf1f96cceef
nestornovella <importarnorth@gmail.com>
2024-04-23 14:17:04 -0300

    changes

--- clients/src/components/location/location.jsx
+++ clients/src/components/location/location.jsx
@@ -27,7 +27,7 @@ function Location() {
             </h2>
 
             <MapContainer center={coordinatePoint} zoom={13} scrollWheelZoom={false} className="h-[700px]">
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"  />
                 <MarkerAppartment selectMarker={centerMarket}/>
             </MapContainer>
         </Transition>
