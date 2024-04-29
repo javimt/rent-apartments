b9105e9180f0c339eeb961fce55d5bd5ef561c44
javimt <javiergp_19@outlook.com>
2024-04-29 18:10:25 -0500

    traducir algunos textos

--- clients/src/components/Auth0Buttons/LoginButton.jsx
+++ clients/src/components/Auth0Buttons/LoginButton.jsx
@@ -9,7 +9,7 @@ function LoginButton() {
       className="px-3 py-2 bg-secondary rounded-lg hover:bg-black text-slate-100 flex justify-center items-center"
       onClick={() => loginWithPopup()}
     >
-      Login <LuLogIn className="ml-1 " />
+      Log In <LuLogIn className="ml-1 " />
     </button>
   );
 }
--- clients/src/components/services/services.jsx
+++ clients/src/components/services/services.jsx
@@ -2,26 +2,24 @@ import useGetApartments from "../../hooks/GetApartments";
 import Transition from "../complements/transition";
 import Slider from "./slider";
 
-
-
-
 function Services() {
 
-    return (
-        <Transition className='grid justify-center px-4 md:py-16 md:px-36 xl:px-48 xl:gap-0  font-quicksand xl:grid-cols-2 md:gap-20'>
-            <div className="max-w-xl mb-2 flex justify-center flex-col ">
-                <h4 className="text-secondary font-semibold ">Services</h4>
-                <h2 className='my-4 text-3xl font-semibold'>Consigue los mejores apartamentos amoblados, unidades modernas y seguras. Sabemos lo que buscas al precio que necesitas</h2>
-                <p className="mb-1 mt-7 text-secondary text-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In error quisquam sit vero natus repellat adipisci velit explicabo provident reprehenderit cum beatae quis ipsam, aliquam sapiente inventore quibusdam veniam. Alias.</p>
-            </div>
-            <div className='  flex justify-center items-center     '>
-
-                <Slider />
-
-            </div>
-        </Transition>
-
-    );
+  return (
+    <Transition className="grid justify-center px-4 md:py-16 md:px-36 xl:px-48 xl:gap-0  font-quicksand xl:grid-cols-2 md:gap-20">
+      <div className="max-w-xl mb-2 flex justify-center flex-col ">
+        <h4 className="text-secondary font-semibold ">Services</h4>
+        <h2 className="my-4 text-3xl font-semibold">
+          Find the best furnished apartments, modern and secure units.
+        </h2>
+        <p className="mb-1 mt-7 text-secondary text-xs">
+          We know what you're looking for at the price you need
+        </p>
+      </div>
+      <div className=" flex justify-center items-center ">
+        <Slider />
+      </div>
+    </Transition>
+  );
 }
 
 export default Services;
