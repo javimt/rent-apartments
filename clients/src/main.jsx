ffb05b59db3bbe5db9b711a0703c9a50dea280b9
javimt <javiergp_19@outlook.com>
2024-05-01 17:03:54 -0500

    baseUrl en main

--- clients/src/main.jsx
+++ clients/src/main.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -5,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/store.js';
 import { Auth0Provider } from '@auth0/auth0-react';
+
+axios.defaults.baseURL = "https://api-rent-appartament.up.railway.app";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
