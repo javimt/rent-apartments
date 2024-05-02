8467a8827f23494c509a17bcf0de6f9d2b5c5581
nestornovella <importarnorth@gmail.com>
2024-05-02 13:47:26 -0300

    changes

--- clients/src/components/admin/createApartment/renderNewApart.jsx
+++ clients/src/components/admin/createApartment/renderNewApart.jsx
@@ -4,7 +4,7 @@ import { LiaBathSolid, LiaBedSolid, LiaRulerCombinedSolid, LiaRulerSolid } from 
 
 
 function RenderNewApartSection({ input }) {
-    const { images, urbanizacion, bathrooms, bedrooms, size, description } = input
+    const { images, urbanizacion, bathrooms, bedrooms, size, description, status } = input
     return (
         <Transition className="flex flex-col items-center h-full shadow-2xl rounded-lg xl:mx-auto p-1 font-quicksand">
             <div>
@@ -14,8 +14,10 @@ function RenderNewApartSection({ input }) {
                 <h2>{urbanizacion ? urbanizacion : 'Title Here'}</h2>
             </div>
             <div className="flex flex-col justify-center mt-2">
-                <div className="w-[400px] h-[250px]  bg-cover bg-center rounded-lg object-fill" style={{ backgroundImage: `url(${images && images[0] ? images[0] : 'https://t3.ftcdn.net/jpg/04/84/88/76/360_F_484887682_Mx57wpHG4lKrPAG0y7Q8Q7bJ952J3TTO.jpg'})` }}>
-
+                <div className="w-[400px] h-[250px]  bg-cover bg-center rounded-lg object-fill relative border" style={{ backgroundImage: `url(${images && images[0] ? images[0] : 'https://t3.ftcdn.net/jpg/04/84/88/76/360_F_484887682_Mx57wpHG4lKrPAG0y7Q8Q7bJ952J3TTO.jpg'})` }}>
+                    <div className={`w-[50px] p-1 px-2 rounded absolute top-0 right-0 text-white ${status == 'rent' ? 'bg-green-400' : 'bg-red-400'}`}>
+                        <span>{status}</span>
+                    </div>
                 </div>
                 <div className="flex gap-2 justify-center">
                     <div className="flex w-[50px] px-2 my-2 py-1 rounded-lg bg-slate-300/30">
