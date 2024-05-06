d2fbb85192fe1c2626748cc26fba4e6a3da7209d
javimt <javiergp_19@outlook.com>
2024-05-06 17:42:50 -0500

    rating and user actions and reducers ok

--- clients/src/redux/reducers/userReducer.js
+++ clients/src/redux/reducers/userReducer.js
@@ -1,12 +1,21 @@
+import { actionTypes } from "../actions/actionTypes";
+
 const initialState = {
   users: [],
 };
 
 function userReducer(state = initialState, action) {
   switch (action.type) {
-    case "":
-      return { ...state };
-
+    case actionTypes.GET_ALL_USERS:
+      return {
+        ...state,
+        users: action.payload,
+      };
+    case actionTypes.GET_ONE_USER_DETAIL:
+      return {
+        ...state,
+        users: action.payload,
+      };
     default:
       return { ...state };
   }
