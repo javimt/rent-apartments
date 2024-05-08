134e8bcea9b70b9c8cf8a8a98526e1243b49b9c1
nestornovella <importarnorth@gmail.com>
2024-05-08 11:24:49 -0300

    change

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({
-  origin: "http://rent-apartments-medellin.up.railway.app/*",
+  origin: "http://rent-apartments-medellin.up.railway.app/",
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   preflightContinue: false,
   optionsSuccessStatus: 204
