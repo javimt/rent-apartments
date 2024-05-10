12e8c6c1221846eb05cdf08630aa116893ee88e8
javimt <javiergp_19@outlook.com>
2024-05-10 18:31:51 -0500

    export function rent from hook rent

--- clients/src/hooks/custom/rentHook.js
+++ clients/src/hooks/custom/rentHook.js
@@ -1,35 +1,30 @@
 import { useState } from "react";
 
-
-
 function useGenerateRent() {
-    
-    const [date, setDate] = useState({start:null, end:null})
-    function dateHandler(e){
-        
-        if(e.type == 'start'){
-            setDate({...date, start: e.date})
-        }else{
-            setDate({...date, end:e.date})
-        }
-
+  const [date, setDate] = useState({ start: null, end: null });
+  function dateHandler(e) {
+    if (e.type == "start") {
+      setDate({ ...date, start: e.date });
+    } else {
+      setDate({ ...date, end: e.date });
     }
+  }
 
-    function generateRent(rent){
-        fetch('', {
-            method:'PUT',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body:JSON.stringify(rent)
-        })
+  function generateRent(rent) {
+    fetch("", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(rent),
+    });
+  }
 
-    }
-    
-    return {
-        date,
-        dateHandler
-    };
+  return {
+    date,
+    dateHandler,
+    generateRent,
+  };
 }
 
 export default useGenerateRent;
