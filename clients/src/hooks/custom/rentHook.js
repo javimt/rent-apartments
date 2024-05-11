ea2c2f4eaf86c04e07aff7290165cde54da50899
javimt <javiergp_19@outlook.com>
2024-05-10 19:57:26 -0500

    rent ok

--- clients/src/hooks/custom/rentHook.js
+++ clients/src/hooks/custom/rentHook.js
@@ -1,27 +1,34 @@
 import { useState } from "react";
 
 function useGenerateRent() {
-  const [date, setDate] = useState({ start: null, end: null });
-  function dateHandler(e) {
-    if (e.type == "start") {
-      setDate({ ...date, start: e.date });
-    } else {
-      setDate({ ...date, end: e.date });
-    }
+  const [dates, setDates] = useState({ start: null, end: null });
+
+  function dateHandler(type, date) {
+    setDates({ ...dates, [type]: date });
   }
 
-  function generateRent(rent) {
-    fetch("", {
+  function generateRent(apartmentId, userId) {
+    const rentData = {
+      apartmentId,
+      userId,
+      startDate: dates.start,
+      endDate: dates.end,
+    };
+
+    fetch("http://localhost:3001/rent", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(rent),
-    });
-  }
+      body: JSON.stringify(rentData),
+    })
+    .then((response) => response.json())
+    .then((info) => console.log(info))
+    .catch(error => console.error(error));
+  } 
 
   return {
-    date,
+    dates,
     dateHandler,
     generateRent,
   };
